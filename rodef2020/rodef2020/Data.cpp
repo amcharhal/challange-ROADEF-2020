72e3b4ead1575a646d585947a938427fd78081fe
Mohamed Amcharhal <MAmcharhal@slb.com>
2020-04-20 12:36:09 +0200

    Add exclusion

--- rodef2020/rodef2020/Data.cpp
+++ rodef2020/rodef2020/Data.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std; 
 #include "Data.h"
-
+#include<string>
 
 Data::Data(JsonMap& ar_json)
 {
@@ -33,20 +33,46 @@ Data::Data(JsonMap& ar_json)
 			m_T = it.value().get<int>();
 		else if (it.key() == "Seasons")
 		{
-			std::vector<int> ar_winter;
-			std::vector<int> ar_summer;
-			std::vector<int> ar_is;
-			std::vector<int> ar_full;
+			std::vector<int> *ar_winter = new std::vector<int>;
+			std::vector<int> *ar_summer = new std::vector<int>;
+			std::vector<int> *ar_is = new std::vector<int>;
+			std::vector<int> *ar_full = new std::vector<int>;
 
 			if (it.value().contains("full"))
-				ar_full = it.value()["full"].get<std::vector<int>>();
+				*ar_full = it.value()["full"].get<std::vector<int>>();
 			else if (it.value().contains("is"))
-				ar_is = it.value()["is"].get<std::vector<int>>();
+				*ar_is = it.value()["is"].get<std::vector<int>>();
 			else if (it.value().contains("summer"))
-				ar_summer = it.value()["summer"].get<std::vector<int>>();
+				*ar_summer = it.value()["summer"].get<std::vector<int>>();
 			else if (it.value().contains("winter"))
-				ar_winter = it.value()["winter"].get<std::vector<int>>();
-			m_seasons = Seasons(ar_winter, ar_summer, ar_is, ar_full);
+				*ar_winter = it.value()["winter"].get<std::vector<int>>();
+			m_seasons = Seasons(*ar_winter, *ar_summer, *ar_is, *ar_full);
+			delete ar_full;
+			delete ar_is;
+			delete ar_summer;
+			delete ar_winter;
+		}
+		else if (it.key() == "Exclusions")
+		{
+			for (json::iterator ip = it.value().begin(); ip != it.value().end(); ++ip)
+			{
+				int m_i1 = stoi(ip.value()[0].get<string>().substr(1));
+				int m_i2 = stoi(ip.value()[1].get<string>().substr(1));
+				string s_season = ip.value()[2].get<string>();
+				E_Season m_season;
+
+				if (s_season == "full")
+					m_season = FULL;
+				else if (s_season == "winter")
+					m_season = WINTER;
+				else if (s_season == "is")
+					m_season = IS;
+				else if (s_season == "summer")
+					m_season = SUMMER;
+
+				Exclusion *ip_exclusion = new Exclusion(m_i1, m_i2, m_season);
+				m_exclusions.push_back(ip_exclusion);
+			}
 		}
 	}
 }
