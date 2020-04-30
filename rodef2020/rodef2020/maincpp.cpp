7d0ca82131fae7c874ae7c93b1aeef4aa30182cc
amcharhal <m.amcharhal@gmail.com>
2020-05-01 00:29:35 +0200

    Large instance test

--- rodef2020/rodef2020/maincpp.cpp
+++ rodef2020/rodef2020/maincpp.cpp
@@ -12,12 +12,12 @@
 using namespace std;
 using json = nlohmann::json;
 int main() {
-	/*srand(static_cast<unsigned int>(time(static_cast<time_t>(NULL))));
-	Simulation senario_1(7, 10);
-	Simulation senario_2(7, 10);
-	vector<vector<int>> ysolution1 = senario_1.getDeltainstance();
-	vector<vector<int>> ysolution2 = senario_2.getXsolution();
-	*/
+	srand(static_cast<unsigned int>(time(static_cast<time_t>(NULL))));
+	Simulation senario_1(500, 356);
+	//Simulation senario_2(7, 10);
+	//vector<vector<int>> ysolution1 = senario_1.getDeltainstance();
+	//vector<vector<int>> ysolution2 = senario_2.getXsolution();
+	
 	/*
 	bool check = true;
 	for (int i = 100; i < 4000; i = i + 100)
@@ -51,11 +51,12 @@ int main() {
 	exemple1Map = exemple1.getJsonMap();
 	cout << exemple1Map->at("Seasons") << endl;*/
 
-	//Instance inst("exemple1.json");
-	//Data *mydata= inst.getData();
-	//cout << mydata->getNbResources()<<endl;
+	/*Instance inst("exemple1.json");
+	Data *mydata= inst.getData();
+	cout << mydata->getNbResources()<<endl;
+	cout << "hello world" << endl;
 	//inst.solve();
-
+	*/
 	
 	return 0;
 }
